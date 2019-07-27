4e33d3be652b9428bb2e0e542b68edb876db820c
mbradshaw21 <35607617+mbradshaw21@users.noreply.github.com>
2019-07-27 13:04:25 -0400

    Add files via upload

---
+++ Activity Organizer/Activity.java
@@ -0,0 +1,11 @@
+package Sorter;
+
+import java.util.*;
+
+public class Activity {
+    public int number, capacity, enrolled;
+    List<Student> roster = new ArrayList<Student>();
+    Activity(int cap) {
+        capacity = cap;
+    }
+}
